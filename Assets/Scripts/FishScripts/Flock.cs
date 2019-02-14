9a9abc6017a17d63748d737ee2b8e716fac9100f
Kyle Lekkas <kylelekkas456@gmail.com>
2019-02-14 10:00:21 -0500

    Simple bobbing to bottle

---
+++ Assets/Scripts/FishScripts/Flock.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour {
+
+    public float speed = 0.1f;
+    float rotationSpeed = 2.0f;
+    Vector3 averageHeading;
+    Vector3 averagePosition;
+    float neighborDistance = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+        speed = Random.Range(0.1f, 0.8f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Random.Range(0, 5) < 1)
+        {
+            ApplyRules();
+        }
+
+        transform.Translate(0, 0, Time.deltaTime * speed);
+    }
+
+    void ApplyRules()
+    {
+        //Get all our fish game objects
+        GameObject[] gos;
+        gos = GlobalFlock.fishes;
+
+        Vector3 vcenter = Vector3.zero;
+        Vector3 vavoid = Vector3.zero;
+        float gSpeed = 0.25f;
+
+        Vector3 goalPos = GlobalFlock.goalPos;
+
+        float dist;
+
+        int groupSize = 0;
+        foreach (GameObject go in gos)
+        {
+            if (go != this.gameObject)
+            {
+                dist = Vector3.Distance(go.transform.position, this.transform.position);
+                if (dist <= neighborDistance)
+                {
+                    vcenter += go.transform.position;
+                    groupSize++;
+
+                    if (dist < 1.0f)
+                    {
+                        vavoid = vavoid + (this.transform.position - go.transform.position);
+                    }
+
+                    Flock anotherFlock = go.GetComponent<Flock>();
+                    gSpeed = gSpeed + anotherFlock.speed;
+                }
+            }
+        }
+
+        if (groupSize > 0)
+        {
+            //calc average center and speed
+            vcenter = vcenter / groupSize + (goalPos - this.transform.position);
+            speed = gSpeed / groupSize;
+
+            Vector3 direction = (vcenter + vavoid) - transform.position;
+            if (direction != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                      Quaternion.LookRotation(direction),
+                                                      rotationSpeed + Time.deltaTime);
+            }
+        }
+
+
+
+    }
+}
---
+++ Assets/Scripts/FishScripts/GlobalFlock.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalFlock : MonoBehaviour {
+
+    //MAIN CAMERA
+    private GameObject myCam;
+
+    //FISH
+    public GameObject fishPrefab;
+    //Spawn space
+    public static int seaSize = 5;
+
+    //Number of fish to spawn, preset for now
+    static int numFish = 20;
+
+    //Array of all our fishies
+    public static GameObject[] fishes = new GameObject[numFish];
+
+    //Goal position for our fish to flock toward
+    public static Vector3 goalPos = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+        //get camera and its location
+        myCam = GameObject.FindGameObjectWithTag("MainCamera");
+        Vector3 camPos = myCam.transform.position;
+
+        //instantiate all fish
+        //at the location of the camera
+		for (int i = 0; i < numFish; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-seaSize * 2 + camPos.x, seaSize * 2 + camPos.x),
+                                       Random.Range(-seaSize + camPos.y, seaSize + camPos.y),
+                                       Random.Range(-seaSize + camPos.z, seaSize + camPos.z));
+            fishes[i] = (GameObject)Instantiate(fishPrefab, pos, Quaternion.identity);
+        }
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//occasionally change our goal position
+        if (Random.Range(0, 1000) < 10)
+        {
+            goalPos = new Vector3(Random.Range(-seaSize, seaSize),
+                                Random.Range(-seaSize, seaSize),
+                                Random.Range(50, 60));
+        }
+	}
+
+    public Vector3 GetGoalPosition()
+    {
+        return goalPos;
+    }
+}
