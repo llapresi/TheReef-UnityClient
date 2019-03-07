1e811a24841b0212a401f4258cef154eff84a90a
Kyle Lekkas <kylelekkas456@gmail.com>
2019-03-06 22:55:47 -0500

    Trash falls from top of screen, timer displayed on canvas

--- Assets/Scripts/ItemSpawning.cs
+++ Assets/Scripts/ItemSpawning.cs
@@ -6,7 +6,18 @@ public class ItemSpawning : MonoBehaviour {
 
     public GameObject bottlePrefab;
     public float spawnRate = 3.0f;
+    public float fallSpeed = 0.5f;
     private float timeSinceLastSpawn = 0.0f;
+
+    public int totalTrashMade = 0;
+    public int totalTrashCollected = 0;
+
+    //Dimensions of screen space
+    float fullWidth;
+    float fullHeight;
+
+    float yRandomRange;
+    float xRandomRange;
 
     public Transform[] spawnPoints;
 
@@ -15,6 +26,7 @@ public class ItemSpawning : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         trashItems = new List<GameObject>();
+        //InitializeSpawnPoints();
 	}
 	
 	// Update is called once per frame
@@ -22,25 +34,90 @@ public class ItemSpawning : MonoBehaviour {
         timeSinceLastSpawn += Time.deltaTime;
 		if (timeSinceLastSpawn >= spawnRate)
         {
-            spawnTrash();
+            SpawnTrash();
             timeSinceLastSpawn = 0.0f;
         }
-        moveTrash();
+        MoveTrash();
 	}
 
-    void spawnTrash()
+    void SpawnTrash()
     {
-        GameObject tempObj = Instantiate(bottlePrefab, new Vector3( 0.0f, 0.0f, 10.0f), Quaternion.identity);
-        trashItems.Add(tempObj);
-    }
+        for (int i = 0; i < spawnPoints.Length; i++)
+        { 
 
-    void moveTrash()
-    {
-        foreach (GameObject trash in trashItems)
-        {
-            Vector3 tempPos = trash.transform.position;
-            tempPos.y -= 0.5f;
-            trash.transform.position = tempPos;
+            GameObject tempObj = Instantiate(bottlePrefab, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity, spawnPoints[i]);
+            //Get Parents location
+            Vector3 randomPosition = spawnPoints[i].position;
+
+            xRandomRange = 15.0f;
+            yRandomRange = 50.0f;
+
+            //Stagger the locations slightly, ideally this would be done with 1/16 of the scene width
+            randomPosition.x += Random.Range((-xRandomRange), xRandomRange);
+            randomPosition.y += Random.value * yRandomRange;
+
+            //Set that to our bottles location
+            tempObj.transform.position = randomPosition;
+
+            //Also give it a slight rotation
+            Vector3 tweakedRotation = tempObj.transform.eulerAngles;
+            tweakedRotation.z = (float)(Random.Range(0, 45) - 22.5);
+            tempObj.transform.eulerAngles = tweakedRotation;
+
+            //Add it to our list
+            trashItems.Add(tempObj);
+
+            //Incriment our trash made by 1 for each item made
+            //Whenever a trash is destroyed, we'll add it to totalTrashCollected
+            //at the end of the game we'll use the two to determine the % of the reef
+            totalTrashMade++;
         }
     }
+
+    void MoveTrash()
+    {
+        for ( int i = trashItems.Count - 1; i >= 0; i--)
+        { 
+            Vector3 tempPos = trashItems[i].transform.position;
+            tempPos.y -= fallSpeed;
+            trashItems[i].transform.position = tempPos;
+
+            //So we dont loop twice, just destroy trash in here if it falls too far down
+            if (tempPos.y <= -50)
+            {
+                Destroy(trashItems[i]);
+                trashItems.RemoveAt(i);
+            }
+
+        }
+    }
+
+    //Some fancy positioning for our quadrants on start, but canvas is offset from the camera so the positions
+    //dont exactly line up the way they should. will have to tweak. until then.... hardcode positions in scene
+    //...just realized itd be easier to not have the spawnPoints centered. too late for that
+    void InitializeSpawnPoints()
+    {
+        //Get the Size of the space we're working with
+        GameObject uiCanvas = GameObject.Find("Canvas");
+        RectTransform canvasSize = uiCanvas.GetComponent<RectTransform>();
+
+        fullWidth = canvasSize.rect.width;
+        fullHeight = canvasSize.rect.height;
+
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            //Want our Spawn Points separated into quarters
+            //1st, 3rd, 5th, and 7th eigth to center them
+            //Have to handle first case differently
+            float signChange = -1;
+            if (i == 0)
+            {
+                signChange = 1;
+            }
+
+            float xPos = (fullWidth / 8) * ((i * 2) + (1 * signChange));
+
+            spawnPoints[i].position = new Vector3(xPos, fullHeight/2, this.transform.position.z);
+        }
+    }//End Initialize Spawn Points
 }
---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour {
+
+    public Text timerText;
+
+    public float time = 90; //default 90 seconds for now
+	
+	// Update is called once per frame
+	void Update () {
+        time -= Time.deltaTime;
+
+        float minutes = time / 60;
+        float seconds = time % 60;
+
+        //update label value
+        timerText.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+
+        EndGame();
+    }
+
+    void EndGame()
+    {
+        if (time <= 0)
+        {
+            //The game ends, so, do end game stuff
+        }
+        else if (time <= 30)
+        {
+            //Increase fall speed prolly
+        }
+
+    }
+}
