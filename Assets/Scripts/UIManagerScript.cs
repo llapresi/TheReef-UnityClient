e0a789cfeca4ace16baf42cc813e9f89c99a9c80
Kyle Lekkas <kylelekkas456@gmail.com>
2019-03-27 00:07:51 -0400

    Added text transitions class as well as some text transitions

--- Assets/Scripts/UIManagerScript.cs
+++ Assets/Scripts/UIManagerScript.cs
@@ -6,23 +6,34 @@ using UnityEngine.UI;
 public class UIManagerScript : MonoBehaviour {
 
     private float totalTime;
+    private List<TextTransition> textTransitions;
 
-    //Welcome To
-    public Text introText;
-    //The Reef
-    public Text introTextTwo;
+    
+    public Text introText;    //Welcome To
+    public Text introTextTwo; //The Reef
+    public Text descText;     //11.1 billion
+    public Text descTextTwo;  //You can change
+    public Text prompt;       //Collect plastic
+
 
     public class TextTransition
     {
         public Text text;
         public bool shouldShow;
         public bool shouldTransition;
+        public float startTime;
+        public float endTime;
 
-        public TextTransition(Text p_text)
+        public TextTransition(Text p_text,float p_startTime, float p_endTime)
         {
             text = p_text;
             shouldShow = true;
             shouldTransition = true;
+            startTime = p_startTime;
+            endTime = p_endTime;
+
+            //Set intial alpha to 0
+            text.CrossFadeAlpha(0.0f, 0.0f, false);
         }
 
         public void FadeIn(float duration)
@@ -47,10 +58,14 @@ public class UIManagerScript : MonoBehaviour {
     // Use this for initialization
     void Start () {
         totalTime = 0.0f;
-
-        //Initialize our classes
-         intro1 = new TextTransition(introText);
-         intro2 = new TextTransition(introTextTwo);
+        textTransitions = new List<TextTransition>();
+        
+        //(Text to display, time to enter, time to leave)
+        textTransitions.Add(new TextTransition(introText, 1.0f, 5.0f));
+        textTransitions.Add(new TextTransition(introTextTwo, 2.0f, 5.0f));
+        textTransitions.Add(new TextTransition(descText, 7.0f, 11.0f));
+        textTransitions.Add(new TextTransition(descTextTwo, 12.0f, 16.0f));
+        textTransitions.Add(new TextTransition(prompt, 17.0f, 22.0f));
 
     }
 	
@@ -64,19 +79,25 @@ public class UIManagerScript : MonoBehaviour {
     void ManageTransitions()
     {
         totalTime += Time.deltaTime;
-        
-        if (intro1.shouldTransition)
+
+        foreach( TextTransition t in textTransitions)
         {
-            if (totalTime >= 2.0f && intro1.shouldShow)
+            if (t.shouldTransition)
             {
-                intro1.FadeIn(1.0f);
-                intro1.shouldShow = false;
-            }
-            if (totalTime >= 5.0)
-            {
-                intro1.FadeOut(1.0f);
-                intro1.IsDone();
+                if (totalTime >= t.startTime && t.shouldShow)
+                {
+                    t.FadeIn(1.0f);
+                    t.shouldShow = false;
+                    Debug.Log("Enter 1st text");
+                }
+                if (totalTime >= t.endTime)
+                {
+                    t.FadeOut(1.0f);
+                    t.IsDone();
+                    Debug.Log("Leave 1st text");
+                }
             }
         }
+        
     }
 }
