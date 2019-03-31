6480762ad08d689daba5aaf9d6cd26cfecd0630c
Kyle Lekkas <kylelekkas456@gmail.com>
2019-03-31 16:14:35 -0400

    Converted to UI Transitions class to transition images and texts alike

--- Assets/Scripts/UIManagerScript.cs
+++ Assets/Scripts/UIManagerScript.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class UIManagerScript : MonoBehaviour {
 
     private float totalTime;
-    private List<TextTransition> textTransitions;
+    private List<UITransition> uiTransitions;
 
     
     public Text introText;    //Welcome To
@@ -14,36 +14,38 @@ public class UIManagerScript : MonoBehaviour {
     public Text descText;     //11.1 billion
     public Text descTextTwo;  //You can change
     public Text prompt;       //Collect plastic
+    public Text final;        //Journey
+
+    public Image bottle;
+    public Image bubble;
+    public Image phone;
 
 
-    public class TextTransition
+    public class UITransition
     {
-        public Text text;
+        //public Text text;
         public bool shouldShow;
         public bool shouldTransition;
         public float startTime;
         public float endTime;
 
-        public TextTransition(Text p_text,float p_startTime, float p_endTime)
+        public UITransition(float p_startTime, float p_endTime)
         {
-            text = p_text;
+            //text = p_text;
             shouldShow = true;
             shouldTransition = true;
             startTime = p_startTime;
             endTime = p_endTime;
-
-            //Set intial alpha to 0
-            text.CrossFadeAlpha(0.0f, 0.0f, false);
         }
 
-        public void FadeIn(float duration)
+        public virtual void FadeIn(float duration)
         {
-            text.CrossFadeAlpha(1.0f, duration, false);
+            //text.CrossFadeAlpha(1.0f, duration, false);
         }
 
-        public void FadeOut(float duration)
+        public virtual void FadeOut(float duration)
         {
-            text.CrossFadeAlpha(0.0f, duration, false);
+            //text.CrossFadeAlpha(0.0f, duration, false);
         }
 
         public void IsDone()
@@ -52,25 +54,76 @@ public class UIManagerScript : MonoBehaviour {
         }
     }
 
-    private TextTransition intro1;
-    private TextTransition intro2;
+    public class TextTransition : UITransition
+    {
+        public Text text;
 
-    // Use this for initialization
-    void Start () {
-        totalTime = 0.0f;
-        textTransitions = new List<TextTransition>();
-        
-        //(Text to display, time to enter, time to leave)
-        textTransitions.Add(new TextTransition(introText, 1.0f, 5.0f));
-        textTransitions.Add(new TextTransition(introTextTwo, 2.0f, 5.0f));
-        textTransitions.Add(new TextTransition(descText, 7.0f, 11.0f));
-        textTransitions.Add(new TextTransition(descTextTwo, 12.0f, 16.0f));
-        textTransitions.Add(new TextTransition(prompt, 17.0f, 22.0f));
+        public TextTransition(Text p_text, float p_startTime, float p_endTime) : base(p_startTime, p_endTime)
+        {
+            text = p_text;
+
+            //Set intial alpha to 0
+            text.CrossFadeAlpha(0.0f, 0.0f, false);
+        }
+
+        public override void FadeIn(float duration)
+        {
+            text.CrossFadeAlpha(1.0f, duration, false);
+        }
+
+        public override void FadeOut(float duration)
+        {
+            text.CrossFadeAlpha(0.0f, duration, false);
+        }
 
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    public class ImageTransition : UITransition
+    {
+        public Image image;
+
+        public ImageTransition(Image p_image, float p_startTime, float p_endTime) : base(p_startTime, p_endTime)
+        {
+            image = p_image;
+
+            //Set intial alpha to 0
+            image.CrossFadeAlpha(0.0f, 0.0f, false);
+        }
+
+        public override void FadeIn(float duration)
+        {
+            image.CrossFadeAlpha(1.0f, duration, false);
+        }
+
+        public override void FadeOut(float duration)
+        {
+            image.CrossFadeAlpha(0.0f, duration, false);
+        }
+
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+        totalTime = 0.0f;
+        uiTransitions = new List<UITransition>();
+
+        //(Text to display, time to enter, time to leave)
+        uiTransitions.Add(new TextTransition(introText, 1.0f, 5.0f));
+        uiTransitions.Add(new TextTransition(introTextTwo, 2.0f, 5.0f));
+        uiTransitions.Add(new TextTransition(descText, 7.0f, 11.0f));
+        uiTransitions.Add(new TextTransition(descTextTwo, 12.0f, 16.0f));
+        uiTransitions.Add(new TextTransition(prompt, 17.0f, 22.0f));
+        uiTransitions.Add(new TextTransition(final, 31.0f, 35.0f));
+
+        uiTransitions.Add(new ImageTransition(bottle, 23.0f, 30.0f));
+        uiTransitions.Add(new ImageTransition(bubble, 23.0f, 30.0f));
+        uiTransitions.Add(new ImageTransition(phone, 23.0f, 30.0f));
+        
+        // Update is called once per frame
+    }
+    void Update () {
         ManageTransitions();
     }
 
@@ -80,7 +133,7 @@ public class UIManagerScript : MonoBehaviour {
     {
         totalTime += Time.deltaTime;
 
-        foreach( TextTransition t in textTransitions)
+        foreach (UITransition t in uiTransitions)
         {
             if (t.shouldTransition)
             {
@@ -88,16 +141,15 @@ public class UIManagerScript : MonoBehaviour {
                 {
                     t.FadeIn(1.0f);
                     t.shouldShow = false;
-                    Debug.Log("Enter 1st text");
                 }
                 if (totalTime >= t.endTime)
                 {
                     t.FadeOut(1.0f);
                     t.IsDone();
-                    Debug.Log("Leave 1st text");
                 }
             }
         }
         
-    }
+        
+    }//End Manage Transitions
 }
