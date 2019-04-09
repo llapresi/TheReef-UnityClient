fb006487957a6aed18f0844395e05bfa64d60c9b
Kyle Lekkas <kylelekkas456@gmail.com>
2019-04-09 11:30:03 -0400

    Added new coral assets and reef to Game scene. Added foundation for coral management script

---
+++ Assets/Scripts/CoralManaging.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoralManaging : MonoBehaviour {
+
+    public GameObject itemManager;
+    private ItemSpawning itemManagerScript;
+    private int trashToBeMade;
+    private float percentageCollected;
+
+	// Use this for initialization
+	void Start () {
+        itemManagerScript = itemManager.GetComponent<ItemSpawning>();
+        trashToBeMade = itemManagerScript.trashToSpawn;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ManagePercentageCollected();
+	}
+
+    void ManagePercentageCollected()
+    {
+        //Get a percentage by constantly dividing our trash collected by the total trash to be collected
+        percentageCollected = ((float)itemManagerScript.totalTrashCollected / (float)trashToBeMade);
+    }
+}
